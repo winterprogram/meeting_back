fd78b673ce3072304d4bba063081a3c7c2eb5ddb
winterprogram <chakladar.sandeep4@gmail.com>
2020-04-22 18:45:37 +0530

    adding socket lib

--- routes/routers.js
+++ routes/routers.js
@@ -4,6 +4,7 @@ const signup = require('./../controller/signup')
 const login = require('./../controller/login')
 const adminsign = require('./../controller/adminusersignup')
 const adminlogin = require('./../controller/loginadmin')
+const getall = require('./../controller/getalluser')
 
 let route = (app) => {
 
@@ -15,6 +16,8 @@ let route = (app) => {
     // admin signup
     app.post('/adminsignup', adminsign.usersignupadmin)
     app.post('/adminlogin', adminlogin.adminlogin)
+    // get all user
+    app.get('/getalluser',getall.getalluser)
 }
 
 module.exports = {
